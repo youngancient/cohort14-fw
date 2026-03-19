2c655a895b191f8a50e621f5daca8173e86be0ed
Jaiyeola Akinjide <jaiyeolawety705@gmail.com>
2026-03-19 20:00:19 +0100

    update

--- GroupB/src/components/MainPage.tsx
+++ GroupB/src/components/MainPage.tsx
@@ -1,23 +1,16 @@
-// import { useState } from 'react'
 import ERC20Page from './ERC20page'
 import TokenRegistry from './TokenRegistry'
-// import SplashPage from './SplashPage'
+import DeploymentChart from './DeploymentChart'
 
 export default function MainPage() {
-  // const [splashDone, setSplashDone] = useState(false)
-
-  // if (!splashDone) {
-  //   return (
-  //     <div className="w-full h-screen">
-  //       <SplashPage onComplete={() => setSplashDone(true)} />
-  //     </div>
-  //   )
-  // }
-
   return (
     <div className="w-full flex flex-col">
       <section className="w-full min-h-screen relative flex-shrink-0 overflow-hidden">
         <ERC20Page />
+      </section>
+
+      <section className="relative z-10 w-full flex-shrink-0" style={{ background: '#05050d' }}>
+        <DeploymentChart />
       </section>
 
       <section className="relative z-10 w-full flex-shrink-0 py-16" style={{ background: '#05050d' }}>
