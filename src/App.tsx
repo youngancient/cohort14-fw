eccbc05d49d8a160133e40c223299879909947c6
LordDregg <oluwatosinodejide121@gmail.com>
2026-03-19 16:40:00 +0100

    pull from development

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import RegistrySection from "./components/RegistrySection";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Hero/>
+      <RegistrySection/>
       <Footer/>
     </>
   );
