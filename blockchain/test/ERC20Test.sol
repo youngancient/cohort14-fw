1b357bdc3531a0a553bdc1de6aa3fc4a934c627a
LordDregg <oluwatosinodejide121@gmail.com>
2026-03-19 03:08:44 +0100

    feat: transfer test

--- blockchain/test/ERC20Test.sol
+++ blockchain/test/ERC20Test.sol
@@ -6,19 +6,38 @@ import {ERC20} from "src/ERC20.sol";
 
 contract ERC20Test is Test {
   ERC20 token;
+  address user1 = makeAddr("user1");
+  address user2 = makeAddr("user2");
+
   string public constant NAME = "Test";
   string public constant SYMBOL = "TST";
   uint8 public constant DECIMAL = 18;
-
+  uint256 public constant STARTING_AMOUNT = 1000;
 
   function setUp() public {
     token = new ERC20(NAME, SYMBOL, DECIMAL);
+
+    vm.prank(user1);
+    token.mint(user1, STARTING_AMOUNT);
   }
 
-  function testInitialParametersAreSet() public {
+  function test__InitialParametersAreSet() public view {
     assertEq(token.name(), NAME);
     assertEq(token.symbol(), SYMBOL);
     assertEq(token.decimals(), DECIMAL);
-    assert(token.totalSupply() == 0);
+    assert(token.totalSupply() == STARTING_AMOUNT);
+    assert(token.balanceOf(user1) == STARTING_AMOUNT);
+  }
+
+  function test__Transfer() public {
+    vm.prank(user1);
+    token.transfer(user2, STARTING_AMOUNT);
+
+    uint256 user1AfterBalance = token.balanceOf(user1);
+    uint256 user2AfterBalance = token.balanceOf(user2);
+
+    assert(user1AfterBalance == 0);
+    assert(user2AfterBalance == STARTING_AMOUNT);
+  
   }
 }
